2c844bae937ccbc6bb88c49c43f610e55ee7c357
Parth Goel <146020919+ParthGoel01@users.noreply.github.com>
2024-06-15 22:04:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
 });
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 const pgSession = connectPgSimple(session);
