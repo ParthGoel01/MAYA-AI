4b5cdaa9d30a608bb591d63d567b9711838a6f77
Parth Goel <146020919+ParthGoel01@users.noreply.github.com>
2024-06-15 22:10:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ db.connect((err)=>{
   else console.log('database connected successfully');
 })
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname,"public")));
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send('Something went wrong!');
